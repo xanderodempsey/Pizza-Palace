2f2b9342190dc55c4e50b455eda4def94c002dc6
Daniellarmar <daniellarmar@gmail.com>
2017-05-23 08:41:35 +1000

    Add files via upload

--- src/asgn2Pizzas/Pizza.java
+++ src/asgn2Pizzas/Pizza.java
@@ -2,6 +2,8 @@ package asgn2Pizzas;
 
 import java.time.LocalTime;
 
+import asgn2Exceptions.PizzaException;
+
 
 /**
  * An abstract class that represents pizzas sold at the Pizza Palace restaurant. 
@@ -9,7 +11,7 @@ import java.time.LocalTime;
  * Each of these subclasses have a different set of toppings. A description of the class's fields
  * and their constraints is provided in Section 5.1 of the Assignment Specification. 
  * 
- * @author Person A
+ * @author Daniel Larmar
  *
  */
 public abstract class Pizza  {
@@ -31,8 +33,60 @@ public abstract class Pizza  {
 	 * @throws PizzaException if supplied parameters are invalid 
 	 * 
 	 */
-	public Pizza(int quantity, LocalTime orderTime, LocalTime deliveryTime, String type, double price) throws PizzaException{
-		// TO DO	
+	
+	// Globals to be used by other methods
+	
+	private double Cost;
+	private double Price;
+	private String Type;
+	private int Quantity;
+	
+	public Pizza(int quantity, LocalTime orderTime, LocalTime deliveryTime, String type, double price) throws PizzaException
+	{
+		
+		// Test for exceptions
+		if(quantity < 1) // if quantity order is less than 1
+		{
+			
+			throw new PizzaException("At least one pizza must be ordered."); //throw exception
+			
+		}
+		else if(orderTime.isBefore(LocalTime.of(19, 00))) // if the orderTime is before opening hours
+		{
+			
+			throw new PizzaException("Please Order After 7:00pm Local Time"); //throw exception
+			
+		}
+		else if(orderTime.isAfter(LocalTime.of(23, 00))) // If the orderTime is after opening hours
+		{
+			
+			throw new PizzaException("Please Order Before 11:00pm Local Time"); // throw exception
+			
+		}
+		else if(!type.equals("PZL") && !type.equals("PZM") && !type.equals("PZV")) // If the pizza not a valid pizza type
+		{
+			
+			throw new PizzaException(type + " is not a valid pizza name. Please enter a valid pizza type: PZL, PZM or PZV."); // Throw the exception						
+			
+		}
+		else if(quantity > 10) // if the quantity is more than 10
+		{
+			
+			throw new PizzaException("Maximum number of pizzas is 10."); // throw the exception
+			
+		}
+		else // values are valid
+		{
+		
+			// Store globals
+			Quantity = quantity;
+			Type = type;
+			Price = price;
+					
+			//calculates the cost per pizza
+			this.calculateCostPerPizza();		
+		
+		}
 	}
 
 	/**
@@ -41,40 +95,74 @@ public abstract class Pizza  {
      * <P> PRE: TRUE
 	 * <P> POST: The cost field is set to sum of the Pizzas's toppings
 	 */
-	public final void calculateCostPerPizza(){
-		// TO DO
+	public final void calculateCostPerPizza()
+	{
+			
+		// Get all the toppings from the enum
+		PizzaTopping Toppings[] = PizzaTopping.class.getEnumConstants();
+		// Loop through all the toppings
+		for(PizzaTopping topping: Toppings)
+		{
+			
+			// Check if the pizza contains the topping
+			if(this.containsTopping(topping))
+			{
+				
+				// Add the cost of the topping to the total cost
+				Cost += topping.getCost();
+				
+			}
+			
+		}
+		
 	}
 	
 	/**
 	 * Returns the amount that an individual pizza costs to make.
 	 * @return The amount that an individual pizza costs to make.
 	 */
-	public final double getCostPerPizza(){
-		// TO DO
+	public final double getCostPerPizza()
+	{
+		
+		//Returns the cost of the pizza
+		return Cost;
+		
 	}
 
 	/**
 	 * Returns the amount that an individual pizza is sold to the customer.
 	 * @return The amount that an individual pizza is sold to the customer.
 	 */
-	public final double getPricePerPizza(){
-		// TO DO
+	public final double getPricePerPizza()
+	{
+		
+		//Return the price of the pizza
+		return this.Price;
+		
 	}
 
 	/**
 	 * Returns the amount that the entire order costs to make, taking into account the type and quantity of pizzas. 
 	 * @return The amount that the entire order costs to make, taking into account the type and quantity of pizzas. 
 	 */
-	public final double getOrderCost(){
-		// TO DO
+	public final double getOrderCost()
+	{
+		
+		//Return the cost of the order: Cost of 1 pizza * quantity ordered
+		return getCostPerPizza() * this.Quantity;
+		
 	}
 	
 	/**
 	 * Returns the amount that the entire order is sold to the customer, taking into account the type and quantity of pizzas. 
 	 * @return The amount that the entire order is sold to the customer, taking into account the type and quantity of pizzas. 
 	 */
-	public final double getOrderPrice(){
-		// TO DO
+	public final double getOrderPrice()
+	{
+		
+		//Return the order price (pizza * quantity ordered)
+		return getPricePerPizza() * this.Quantity;
+		
 	}
 	
 	
@@ -82,8 +170,12 @@ public abstract class Pizza  {
 	 * Returns the profit made by the restaurant on the order which is the order price minus the order cost. 
 	 * @return  Returns the profit made by the restaurant on the order which is the order price minus the order cost.
 	 */
-	public final double getOrderProfit(){
-		// TO DO
+	public final double getOrderProfit()
+	{
+		
+		// Returns the profit: Price of the order - cost of the order
+		return getOrderPrice() - getOrderCost();
+		
 	}
 	
 
@@ -92,16 +184,79 @@ public abstract class Pizza  {
 	 * @param topping -  A topping as specified in the enumeration PizzaTopping
 	 * @return Returns  true if the instance of Pizza contains the specified topping and false otherwise.
 	 */
-	public final boolean containsTopping(PizzaTopping topping){
-		// TO DO
+	public final boolean containsTopping(PizzaTopping topping)
+	{
+		
+		// Check if the pizza is a meat lover
+		if(this.getPizzaType().equals("PZL"))
+		{
+			
+			// Check if the topping is a topping on the meat lover
+			if(MeatLoversPizza.Toppings.contains(topping))
+			{
+				
+				return true;
+				
+			}
+			else
+			{
+				
+				return false;
+				
+			}
+			
+		}
+		// Check if the pizza is a margherita
+		else if (this.getPizzaType().equals("PZM"))
+		{
+			
+			//Check if the topping is a topping on the margherita
+			if(MargheritaPizza.Toppings.contains(topping))
+			{
+				
+				return true;
+				
+			}
+			else
+			{
+				
+				return false;
+				
+			}
+			
+		}
+		// The pizza has to be a vegetarian as exception would've already been risen
+		else
+		{
+			
+			// Check if the topping is a topping on the vegetarian
+			if(VegetarianPizza.Toppings.contains(topping))
+			{
+				
+				return true;
+				
+			}
+			else
+			{
+			
+				return false;
+				
+			}			
+			
+		}
+		
 	}
 	
 	/**
 	 * Returns the quantity of pizzas ordered. 
 	 * @return the quantity of pizzas ordered. 
 	 */
-	public final int getQuantity(){
-		// TO DO
+	public final int getQuantity()
+	{
+		
+		//Return Quantity
+		return Quantity;
+		
 	}
 
 	/**
@@ -109,8 +264,12 @@ public abstract class Pizza  {
 	 * The valid alternatives are listed in Section 5.1 of the Assignment Specification. 
 	 * @return A human understandable description of the Pizza's type.
 	 */
-	public final String getPizzaType(){
-		// TO DO
+	public final String getPizzaType()
+	{
+		
+		//Returns Pizza type
+		return Type;
+		
 	}
 
 
@@ -125,7 +284,8 @@ public abstract class Pizza  {
 	 *   
 	 */
 	@Override
-	public boolean equals(Object other){
+	public boolean equals(Object other)
+	{
 		Pizza otherPizza = (Pizza) other;
 		return ((this.getCostPerPizza()) == (otherPizza.getCostPerPizza()) &&
 			(this.getOrderCost()) == (otherPizza.getOrderCost())) &&				
