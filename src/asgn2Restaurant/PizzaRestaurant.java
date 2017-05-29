780c95ea696f2103ad0f58c10c31066abbefe414
Daniellarmar <daniellarmar@gmail.com>
2017-05-29 13:48:49 +1000

    Added Author names

--- src/asgn2Restaurant/PizzaRestaurant.java
+++ src/asgn2Restaurant/PizzaRestaurant.java
@@ -9,7 +9,7 @@ import asgn2Exceptions.PizzaException;
 import asgn2Pizzas.Pizza;
 
 /**
- * This class acts as a ‘model’ of a pizza restaurant. It contains an ArrayList of Pizza objects and an ArrayList of  Customer objects.
+ * This class acts as a â€˜modelâ€™ of a pizza restaurant. It contains an ArrayList of Pizza objects and an ArrayList of  Customer objects.
  *  It contains a method that can populate the ArrayLists,  several methods to retrieve information about the ArrayLists and 
  *  a method to reset the array list. Information about the x and y location of the restaurant and the time that first and last 
  *  orders are accepted are listed in Section 5 of the Assignment Specification. 
@@ -17,7 +17,7 @@ import asgn2Pizzas.Pizza;
  *  Any exceptions raised by one of the methods called by this class should be passed to asgn2GUIs.PizzaGUI so that it can be shown to
  *  the user.
  * 
- * @author Person A and Person B
+ * @author Daniel Larmar and Alexander O'Dempsey
  *
  */
 public class PizzaRestaurant {
