59a95029cd64fc0c2d0f6ed20a4a1c0ec8430fc2
Daniellarmar <daniellarmar@gmail.com>
2017-06-02 10:18:04 +1000

    Completed Tests

--- src/asgn2Tests/RestaurantCustomerTests.java
+++ src/asgn2Tests/RestaurantCustomerTests.java
@@ -10,7 +10,7 @@ import asgn2Restaurant.PizzaRestaurant;
  * A class that that tests the methods relating to the handling of Customer objects in the asgn2Restaurant.PizzaRestaurant
  * class as well as processLog and resetDetails.
  * 
- * @author Person A
+ * @author Daniel Larmar
  */
 public class RestaurantCustomerTests 
 {
